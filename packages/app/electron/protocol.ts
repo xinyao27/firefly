cd42715fbf2b269af6f79b6c8f59a0183738ad69
chenyueban <jasonchan0527@gmail.com>
2023-02-07 21:52:06 +0800

    fix(app): 修复部分图片格式不支持

--- packages/app/electron/protocol.ts
+++ packages/app/electron/protocol.ts
@@ -1,34 +1,9 @@
 import path from 'node:path'
 import fs from 'fs-extra'
 import log from 'electron-log'
+import mime from 'mime-types'
 
 export const SCHEMA = 'atom'
-
-const mimeTypes: Record<string, string> = {
-  '.js': 'text/javascript',
-  '.mjs': 'text/javascript',
-  '.html': 'text/html',
-  '.htm': 'text/html',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.svg': 'image/svg+xml',
-  '.ico': 'image/vnd.microsoft.icon',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.map': 'text/plain',
-}
-
-function charset(mimeExt: string) {
-  return ['.html', '.htm', '.js', '.mjs'].includes(mimeExt)
-    ? 'utf-8'
-    : null
-}
-
-function mime(filename: string) {
-  const mimeExt = path.extname(`${filename || ''}`).toLowerCase()
-  const mimeType = mimeTypes[mimeExt]
-  return mimeType ? { mimeExt, mimeType } : { mimeExt: null, mimeType: null }
-}
 
 export async function protocolRequestHandler(req: any, next: any) {
   const reqUrl = (req.url as string).slice(7)
@@ -40,11 +15,12 @@ export async function protocolRequestHandler(req: any, next: any) {
 
   try {
     const buffer = await fs.readFile(reqPath)
-    const { mimeExt, mimeType } = mime(reqFilename)
-    if (mimeType !== null) {
+    const mimeType = mime.lookup(reqFilename)
+    if (mimeType) {
+      const charset = mime.charset(mimeType)
       next({
         mimeType,
-        charset: charset(mimeExt),
+        charset,
         data: buffer,
       })
     }
