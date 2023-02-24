1ec0f4a915af865ecec818487763f687b3167a78
chenyueban <jasonchan0527@gmail.com>
2023-02-24 11:16:25 +0800

    feat: 调整 tree

--- packages/app/src/renderer/components/LeftBar/MessageListView.vue
+++ packages/app/src/renderer/components/LeftBar/MessageListView.vue
@@ -3,18 +3,18 @@ const messageStore = useMessageStore()
 </script>
 
 <template>
-  <div h-full p-4 flex flex-col gap-2>
-    <div flex items-center justify-center gap-2>
+  <div h-full flex flex-col>
+    <div flex items-center justify-center gap-2 p-2>
       <NButton
         quaternary
-        size="tiny"
+        size="small"
         @click="messageStore.createArticle"
       >
         <i i-ri-edit-2-line />
       </NButton>
       <NButton
         quaternary
-        size="tiny"
+        size="small"
         @click="messageStore.createFolder"
       >
         <i i-ri-folder-add-line />
--- packages/app/src/renderer/components/List/List.vue
+++ packages/app/src/renderer/components/List/List.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { TreeOption } from 'naive-ui'
+import type { TreeDropInfo, TreeOption } from 'naive-ui'
 import type { Message } from '~/models/Message'
 
 const messageStore = useMessageStore()
@@ -63,32 +63,64 @@ function handleSelect(
   messageStore.currentMessageId = meta.node?.key as string
 }
 
-// const nodeProps = ({ option }: { option: TreeOption }) => {
-//   return {
-//     onDrop(e: DragEvent) {
-//       console.log(e.dataTransfer?.files, e.dataTransfer?.getData('text'))
-//     },
-//   }
-// }
+function findSiblingsAndIndex(
+  node: TreeOption,
+  nodes?: TreeOption[],
+): [TreeOption[], number] | [null, null] {
+  if (!nodes) return [null, null]
+  for (let i = 0; i < nodes.length; ++i) {
+    const siblingNode = nodes[i]
+    if (siblingNode.key === node.key) return [nodes, i]
+    const [siblings, index] = findSiblingsAndIndex(node, siblingNode.children)
+    if (siblings && index !== null) return [siblings, index]
+  }
+  return [null, null]
+}
+function handleDrop({ node, dragNode, dropPosition }: TreeDropInfo) {
+  const [dragNodeSiblings, dragNodeIndex] = findSiblingsAndIndex(
+    dragNode,
+    data.value,
+  )
+  if (dragNodeSiblings === null || dragNodeIndex === null) return
+  dragNodeSiblings.splice(dragNodeIndex, 1)
+  if (dropPosition === 'inside') {
+    if (node.children) {
+      node.children.unshift(dragNode)
+    }
+    else {
+      node.children = [dragNode]
+    }
+  }
+  else if (dropPosition === 'before') {
+    const [nodeSiblings, nodeIndex] = findSiblingsAndIndex(
+      node,
+      data.value,
+    )
+    if (nodeSiblings === null || nodeIndex === null) return
+    nodeSiblings.splice(nodeIndex, 0, dragNode)
+  }
+  else if (dropPosition === 'after') {
+    const [nodeSiblings, nodeIndex] = findSiblingsAndIndex(
+      node,
+      data.value,
+    )
+    if (nodeSiblings === null || nodeIndex === null) return
+    nodeSiblings.splice(nodeIndex + 1, 0, dragNode)
+  }
+}
 </script>
 
 <template>
-  <div
-    w-full h-full
-    draggable="false"
-  >
-    <NTree
-      class="h-full"
-      virtual-scroll
-      block-line
-      expand-on-click
-      draggable
-      selectable
-      cascade
-      :data="data"
-      @update:selected-keys="handleSelect"
-      @dragstart="handleDragStart"
-      @dragend="handleDragEnd"
-    />
-  </div>
+  <NTree
+    class="h-[calc(100%-22px)]"
+    block-line
+    expand-on-click
+    draggable
+    selectable
+    :data="data"
+    @update:selected-keys="handleSelect"
+    @dragstart="handleDragStart"
+    @dragend="handleDragEnd"
+    @drop="handleDrop"
+  />
 </template>
