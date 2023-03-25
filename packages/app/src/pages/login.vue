b2eecdc80fe76e361bc3a22878b940446627626c
chenyueban <jasonchan0527@gmail.com>
2023-03-25 22:02:46 +0800

    feat: 登录方式支持 login code

--- packages/app/src/pages/login.vue
+++ packages/app/src/pages/login.vue
@@ -4,10 +4,21 @@ import { useMessage } from 'naive-ui'
 import { supabase } from '~/api'
 
 const { t } = useI18n()
+const router = useRouter()
 const message = useMessage()
 const formRef = ref<FormInst | null>(null)
 const formValue = ref({
   email: '',
+  loginCode: '',
+})
+const options = computed(() => {
+  return ['@gmail.com', '@163.com', '@qq.com'].map((suffix) => {
+    const prefix = formValue.value.email.split('@')[0]
+    return {
+      label: prefix + suffix,
+      value: prefix + suffix,
+    }
+  })
 })
 const rules = {
   email: {
@@ -19,18 +30,36 @@ const rules = {
 
 const loading = ref(false)
 async function signInWithGithub() {
-  loading.value = true
-  await supabase.auth.signInWithOAuth({
-    provider: 'github',
-  })
-  loading.value = false
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+    if (error)
+      throw error
+  }
+  catch (error: any) {
+    message.error(error.message || error.msg || error)
+  }
+  finally {
+    loading.value = false
+  }
 }
 async function signInWithNotion() {
-  loading.value = true
-  await supabase.auth.signInWithOAuth({
-    provider: 'notion',
-  })
-  loading.value = false
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'notion',
+    })
+    if (error)
+      throw error
+  }
+  catch (error: any) {
+    message.error(error.message || error.msg || error)
+  }
+  finally {
+    loading.value = false
+  }
 }
 const emailSended = ref(false)
 function signInWithOtp() {
@@ -40,11 +69,40 @@ function signInWithOtp() {
       supabase.auth.signInWithOtp({
         email: formValue.value.email,
       })
-        .then(() => {
+        .then(({ error }) => {
+          if (error)
+            throw error
           emailSended.value = true
         })
         .catch((error) => {
-          message.error(error.message)
+          message.error(error.message || error.msg || error)
+        })
+        .finally(() => {
+          loading.value = false
+        })
+    }
+    else {
+      message.error(errors[0]?.[0]?.message ?? t('login.emailRequired'))
+    }
+  })
+}
+function signInWithLoginCode() {
+  formRef.value?.validate((errors) => {
+    if (!errors) {
+      loading.value = true
+      supabase.auth.verifyOtp({
+        email: formValue.value.email,
+        token: formValue.value.loginCode,
+        type: 'magiclink',
+      })
+        .then(({ data, error }) => {
+          if (data.session)
+            router.push('/')
+          else
+            throw error
+        })
+        .catch((error) => {
+          message.error(error.message || error.msg || error)
         })
         .finally(() => {
           loading.value = false
@@ -99,12 +157,24 @@ function signInWithOtp() {
           class="mb-2"
           path="email"
         >
-          <NInput
+          <NAutoComplete
             v-model:value="formValue.email"
             :placeholder="t('login.emailPlaceholder')"
             clearable
+            :options="options"
           />
         </NFormItem>
+        <NButton
+          v-if="!emailSended"
+          block
+          class="mb-2"
+          type="primary"
+          secondary
+          :loading="loading"
+          @click="signInWithOtp"
+        >
+          {{ t('login.continueWithEmail') }}
+        </NButton>
 
         <NFormItem
           v-if="emailSended"
@@ -114,15 +184,27 @@ function signInWithOtp() {
             {{ t('login.emailSended') }}
           </div>
         </NFormItem>
-
+        <NFormItem
+          v-if="emailSended"
+          class="mb-2"
+          path="loginCode"
+        >
+          <NInput
+            v-model:value="formValue.loginCode"
+            :placeholder="t('login.loginCodePlaceholder')"
+            clearable
+            maxlength="6"
+          />
+        </NFormItem>
         <NButton
+          v-if="emailSended"
           block
           type="primary"
           secondary
           :loading="loading"
-          @click="signInWithOtp"
+          @click="signInWithLoginCode"
         >
-          {{ t('login.continueWithEmail') }}
+          {{ t('login.continueWithLoginCode') }}
         </NButton>
       </NForm>
     </section>
