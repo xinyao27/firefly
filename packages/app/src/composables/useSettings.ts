4410f7a86e704f92623c63dd64435b928e8b3aee
chenyueban <jasonchan0527@gmail.com>
2023-04-02 23:42:19 +0800

    更新useSettings.ts，根据设置中的热键和OCR热键绑定快捷键。

--- packages/app/src/composables/useSettings.ts
+++ packages/app/src/composables/useSettings.ts
@@ -11,9 +11,10 @@ export function useSettings() {
     if (is.desktop()) {
       const user = await getUser()
       if (user) {
-        bindHotkey()
-        if (is.macOS())
-          bindOCRHotkey()
+        if (settings.value.hotkey)
+          bindHotkey(settings.value.hotkey)
+        if (is.macOS() && settings.value.ocrHotkey)
+          bindOCRHotkey(settings.value.ocrHotkey)
       }
     }
   })
