08540932fa070afc84ccf8f8a7c464321416624e
chenyueban <jasonchan0527@gmail.com>
2023-04-12 10:13:48 +0800

    chore: Add Sentry replay configuration for better error analysis and debugging in production

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -39,12 +39,22 @@ if (import.meta.env.PROD) {
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
         tracePropagationTargets: ['firefly.best', /^\//],
       }),
+      new Sentry.Replay({
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
     ],
     environment: 'production',
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.6,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 0.6,
   })
 }
 
