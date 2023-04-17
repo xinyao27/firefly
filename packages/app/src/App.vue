fd27cd99fae10129a4a30237a380a96e902d1768
chenyueban <jasonchan0527@gmail.com>
2023-04-17 14:13:55 +0800

    chore: Add SEO metadata using useSeoMeta hook in App.vue

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -12,6 +12,14 @@ useHead({
     },
   ],
 })
+useSeoMeta({
+  title: 'Firefly',
+  description: 'Firefly is a fragment information collection tool, which combines the data analysis ability of AI to make information collection so interesting',
+  ogDescription: 'Firefly is a fragment information collection tool, which combines the data analysis ability of AI to make information collection so interesting',
+  ogTitle: 'Firefly',
+  ogImage: 'https://firefly.best/icon.png',
+  twitterCard: 'summary_large_image',
+})
 
 const settings = useSettings()
 
