b881a6aa542f04c15c1ea382cf6854cedc73d2cd
chenyueban <jasonchan0527@gmail.com>
2023-05-24 14:31:55 +0800

    feat: Add PricingProvider to App.vue and Pricing components

---
+++ packages/app/src/components/Pricing/data.ts
@@ -0,0 +1,23 @@
+export const pricing = [
+  {
+    title: 'Tall Copilot Quotas',
+    newPrice: 3.49,
+    oldPrice: 2.99,
+    descriptions: ['Quota for conversing with AI 200 / 200 次 AI 对话'],
+    link: 'https://firefly.lemonsqueezy.com/checkout/buy/427b96d4-f47d-4607-bbd8-ed7127f37fc5',
+  },
+  {
+    title: 'Grande Copilot Quotas',
+    newPrice: 5.99,
+    oldPrice: 4.99,
+    descriptions: ['Quota for conversing with AI 400 / 400 次 AI 对话'],
+    link: 'https://firefly.lemonsqueezy.com/checkout/buy/968271cc-efa6-4095-bb30-e250fe1a1720',
+  },
+  {
+    title: 'Venti Copilot Quotas',
+    newPrice: 9.99,
+    oldPrice: 11.49,
+    descriptions: ['Quota for conversing with AI 1000 / 1000 次 AI 对话'],
+    link: 'https://firefly.lemonsqueezy.com/checkout/buy/eb579e2a-516a-47ee-9319-7b56f7bbd164',
+  },
+]
